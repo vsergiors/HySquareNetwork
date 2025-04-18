2ee0407b87b3150bee2cdba790616f098b73bc6f
vsergiors <vsergiobusiness@gmail.com>
2025-04-18 11:46:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+
+document.getElementById("app").innerText = "Bienvenido a HySquare";
---
+++ users.js
@@ -0,0 +1,7 @@
+
+const users = [
+  { username: "Sergio", password: "vSergiors", role: "admin" },
+  { username: "Lethanix", password: "1234", role: "staff" },
+  { username: "Venelson", password: "1234", role: "staff" },
+  { username: "Player1", password: "1234", role: "usuario" }
+];
